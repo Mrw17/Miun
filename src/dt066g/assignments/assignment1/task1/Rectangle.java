74b17ff0a7536bdaef0f7140b3b6f60d29951b51
Mrw17 <danielwesterlund17@gmail.com>
2021-04-02 04:06:12 +0200

    Created Rectangle-class

---
+++ src/dt066g/assignments/assignment1/task1/Rectangle.java
@@ -0,0 +1,84 @@
+package dt066g.assignments.assignment1.task1;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * @Author Daniel Westerlund
+ * @Version 1.0
+ * Rectangle class that will extend Paintobject
+ * It will randomize a size between MIN_SIZE and MAX_SIZE
+ * It will randomize a color to the rectangle
+ * It will randomize if the rectangle will be filled or not
+ */
+
+public class Rectangle extends PaintObject {
+    private static final int MIN_SIZE = 10;
+    private static final int MAX_SIZE = 100;
+    int size;
+    boolean filled = false;
+    Color color;
+
+    /**
+     * Constructor that takes x,y-coordinates
+     * @param x
+     * @param y
+     */
+    public Rectangle(int x, int y) {
+        super(x, y, MAX_SIZE, MIN_SIZE);
+        fixColor();
+        fixFilled();
+        fixSize();
+    }
+
+    /**
+     * Draws a rectangle based on attributes
+     * @param g
+     */
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        if(filled)
+            g.fillRect(x,y,size,size);
+        else
+            g.drawRect(x,y,size,size);
+    }
+
+    /**
+     * Randomize size of the triangle between
+     * MIN_SIZE and MAX_SIZE
+     */
+    private void fixSize(){
+        size = generateRandomNumber(MAX_SIZE, MIN_SIZE);
+    }
+
+    /**
+     * Randomize a color of the triangle
+     */
+    private void fixColor(){
+        int red = generateRandomNumber(255,0);
+        int green = generateRandomNumber(255,0);
+        int blue = generateRandomNumber(255,0);
+        this.color = new Color(red,green,blue);
+    }
+
+    /**
+     * Randomize if polygon should be filled or not.
+     */
+    private void fixFilled(){
+        if(generateRandomNumber(1,0) == 1)
+            filled = true;
+    }
+
+    /**
+     * Randomize a number between max and min (inc)
+     * @param max max number (inc)
+     * @param min min number (inc)
+     * @return a randomize integer
+     */
+    private int generateRandomNumber(int max, int min){
+        Random rand = new Random();
+        return rand.nextInt((max-min + 1) + min);
+    }
+
+}
